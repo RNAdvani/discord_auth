2d182c0d3514ab7cf455abf4410f78d69a0b40b6
rohan <advanirohan03@gmail.com>
2024-08-25 01:43:02 +0530

    Update Link

--- auth.ts
+++ auth.ts
@@ -8,7 +8,7 @@ export const { handlers, signIn, signOut, auth } = NextAuth({
     Discord({
         clientId: process.env.DISCORD_CLIENT_ID,
         clientSecret: process.env.DISCORD_CLIENT_SECRET,
-        authorization : "https://discord.com/oauth2/authorize?client_id=1276798228016271361&response_type=code&redirect_uri=http%3A%2F%2Flocalhost%3A3000%2Fapi%2Fauth%2Fcallback%2Fdiscord&scope=identify+guilds+email"
+        authorization : process.env.DISCORD_AUTHORIZATION,
     })
   ],
   session : { strategy :"jwt", maxAge: 30 * 24 * 60 * 60 },
